483da29d954f4e7a9da51b574204f2d39b9321b1
ivanstewart2001 <geoffreystewart2001@gmail.com>
2021-09-07 15:29:37 -0400

    Flask Added

--- src/components/Bills/bills.js
+++ src/components/Bills/bills.js
@@ -1,11 +1,21 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 
-class Bills extends Component {
-    render(){
-        return(
+function Bills() {
+    const [initialData, setInitialData] = useState([{}])
+
+    useEffect(() => {
+        fetch('/backend_to_frontend').then(
+            response => response.json()
+        ).then(data => setInitialData(data))
+    }, [])
+
+    return(
+        <div>
             <div>Bills Page</div>
-        )
-    }
+            <h1>{{initialData}}</h1>
+        </div>
+    )
+    
 }
 
 export default Bills
