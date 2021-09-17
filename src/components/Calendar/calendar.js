31ec1b5f00005e8e5796903b1b6fc15dab45361e
ivanstewart2001 <geoffreystewart2001@gmail.com>
2021-09-16 20:06:12 -0400

    Calendar Updates-Laurens Commit

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,37 +1,56 @@
 import React, {Component} from "react";
 import Paper from '@material-ui/core/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
+import {connect} from "react-redux"
 
 import {
     Scheduler,
     DayView,
     Appointments,
     MonthView,
+    WeekView,
+    TodayButton,
+    ViewSwitcher,
+    Toolbar,
+    DateNavigator,
 } from '@devexpress/dx-react-scheduler-material-ui'
+import moment from "moment"
 
-const currentDate = '2018-11-01';
-const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
 
 class Calendar extends Component {
     render(){
+        const currentDate = moment().format()
+        console.log(this.props.Bills)
+        console.log(this.props.Buckets)
         return(
             <div>
                 <h3>Calendar Page</h3>
                 <Paper>
                     <Scheduler
-                    data={schedulerData}
+                   // data={data}
+                   // height={660}
                     >
                     <ViewState
                         currentDate={currentDate}
+                        onCurrentDateChange={this.onCurrentDateChange}
+                    />
+                    <DayView
+                        startDayHour={9}
+                        endDayHour={18}
+                     />
+                    <WeekView
+                        startDayHour={9}
+                        endDayHour={18}
                     />
                     <MonthView
                         startDayHour={9}
                         endDayHour={14}
                     />
                     <Appointments />
+                    <Toolbar/>
+                    <ViewSwitcher/>
+                    <TodayButton/>
+                    <DateNavigator/>
                     </Scheduler>
                 </Paper>
             </div>
@@ -39,4 +58,10 @@ class Calendar extends Component {
     }
 }
 
-export default Calendar
+const mapStateToProps = (state) => {
+    return {
+       Buckets : state.buckets, Bills: state.bills
+    }
+}
+
+export default connect(mapStateToProps)(Calendar)
