1346eeb9792a4d9e4389f2f568e53f87f2b0bb7c
Lauren <lauren.guthrie@bison.howard.edu>
2021-09-20 20:00:06 -0400

    income

---
+++ src/components/Income/incomeForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import moment from 'moment'
+import 'react-dates/initialize'
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css'
+
+class IncomeForm extends Component {
+
+}
+export default IncomeForm
