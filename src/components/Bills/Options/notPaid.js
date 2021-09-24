1e3e08a29e9e91c92e625ddfbd6778088cf787ba
ivanstewart2001 <geoffreystewart2001@gmail.com>
2021-09-24 13:04:12 -0400

    Logic added so that filtering also changes total

--- src/components/Bills/Options/notPaid.js
+++ src/components/Bills/Options/notPaid.js
@@ -2,18 +2,32 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import BillItem from "../billItem";
 import filterBills from '../../../Filters/bills'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 export class NotPaid extends Component {
     constructor (props){
         super(props)
 
         this.state = {
-            filter : this.props.filterBy
+            filter : this.props.filterBy,
+            currentBillTotal : 0,
+            currentPaidBillTotal : 0
         }
     }
 
     render() {
         const bills = filterBills(this.state.filter, this.props.bills)
+
+        this.state.currentBillTotal = 0
+        this.state.currentPaidBillTotal = 0
+
+        bills.map((bill) => {
+            this.state.currentBillTotal += (bill.amount /100)
+            if (bill.paid === 'yes') {
+                this.state.currentPaidBillTotal += (bill.amount /100)
+            }
+        })
+
         return (
             <div>
                 {
@@ -24,6 +38,15 @@ export class NotPaid extends Component {
                         bills.map((bill) => {
                             return <BillItem key={bill.id} {...bill} />;
                         })
+                }
+                {
+                    this.props.bills.length > 0 ? 
+                        <div>
+                            <ProgressBar now={this.state.currentPaidBillTotal} max={this.state.currentBillTotal} label={`$${this.state.currentPaidBillTotal}`} />
+                            <p style={{marginLeft:'90%'}}>Total: ${this.state.currentBillTotal}</p>
+                        </div>
+                    :
+                        <p></p>
                 }
             </div>
         )
--- src/components/Bills/bills.js
+++ src/components/Bills/bills.js
@@ -1,55 +1,18 @@
 import React, {Component} from "react";
-import ProgressBar from 'react-bootstrap/ProgressBar'
 import BillList from "./billList";
-import { connect } from 'react-redux';
 
 class Bills extends Component {
-    constructor(){
-        super()
-
-        this.state = {
-            currentBillTotal : 0,
-            currentPaidBillTotal : 0
-        }
-    }
-
     render() {
-        this.state.currentBillTotal = 0
-        this.state.currentPaidBillTotal = 0
-
-        this.props.bills.map((bill) => {
-            this.state.currentBillTotal += (bill.amount /100)
-            if (bill.paid === 'yes') {
-                this.state.currentPaidBillTotal += (bill.amount /100)
-            }
-        })
-        
         return(
             <div>
                 <h1>Bills</h1>
                 <h3>Month:</h3>
                 <p>If a Bill is marked as Paid and is not set to recur then it will be deleted 7 days after Bill Due Date</p>
                 <BillList />
-                {
-                    this.props.bills.length > 0 ? 
-                        <div>
-                            <ProgressBar now={this.state.currentPaidBillTotal} max={this.state.currentBillTotal} label={`$${this.state.currentPaidBillTotal}`} />
-                            <p style={{marginLeft:'90%'}}>Total: ${this.state.currentBillTotal}</p>
-                        </div>
-                    :
-                        <p></p>
-                }
-
                 <a href="/addBill">Add Bill</a>
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-      bills: state.bills
-    };
-};
-  
-export default connect(mapStateToProps)(Bills);
+export default Bills
